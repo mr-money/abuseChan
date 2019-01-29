4bce31a5be7d40ffea6c71e109b7e62776bd9c0f
qianjing <740363495@qq.com>
2019-01-29 21:38:07 +0800

    后台 头像根目录地址修改

--- module/Admin/controllers/WechatController.php
+++ module/Admin/controllers/WechatController.php
@@ -36,7 +36,7 @@ class WechatController extends CommonController
         $admin = $this->sessionGlobal->get('admin');
 
         //默认头像
-        $admin['avatar'] = empty($admin['avatar']) ? \Yii::$app->request->getHostInfo() . '/AmazeUi/img/user04.png' : UPLOAD_DIR . '/avatar' . $admin['avatar'];
+        $admin['avatar'] = empty($admin['avatar']) ? \Yii::$app->homeUrl . 'AmazeUi/img/user04.png' : UPLOAD_DIR . '/avatar' . $admin['avatar'];
 
         $responseData['admin'] = $admin;
 
