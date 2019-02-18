2b6d0d6b224f4e9218185d011db1ffab96df762b
qianjing <740363495@qq.com>
2019-02-18 10:36:41 +0800

    提交设置信息

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=abusechan',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+    'tablePrefix' => 'abuse_',   //加入前缀名称fc_
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
