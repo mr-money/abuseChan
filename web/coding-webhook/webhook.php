ef5d5507f9a5943002324561237e8456e635b758
qianjing <740363495@qq.com>
2019-03-06 16:44:31 +0800

    添加webhook内容

--- web/coding-webhook/webhook.php
+++ web/coding-webhook/webhook.php
@@ -1,4 +1,22 @@
 <?php
 
-echo 'git web hook';
-return;
+error_reporting(1);
+
+$target = '/www/abuseChan/'; // 生产环境web目录
+
+$token = 'abusechan';
+$wwwUser = 'www';
+$wwwGroup = 'www';
+
+$json = json_decode(file_get_contents('php://input'), true);
+
+if (empty($json['token']) || $json['token'] !== $token) {
+    exit('error request');
+}
+
+$repo = $json['repository']['name'];
+
+$cmd = "cd $target && git pull";
+
+$res = shell_exec($cmd);
+file_put_contents('gitWebhook.log',$res);
