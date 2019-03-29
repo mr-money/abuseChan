b44ac6822c89bc9b4b58434226de32a097ce18ed
qianjing <740363495@qq.com>
2019-03-29 23:22:17 +0800

    修改admin密码

--- module/Admin/views/admin/adminUser.php
+++ module/Admin/views/admin/adminUser.php
@@ -1,11 +1,11 @@
 <?php
 echo $this->render('../common/_layout');
 
-\app\assets\AppAsset::addJs($this,Yii::$app->request->baseUrl."/cropper/js/cropper.min.js");
-\app\assets\AppAsset::addJs($this,Yii::$app->request->baseUrl."/cropper/js/script.js");
+\app\assets\AppAsset::addJs($this, Yii::$app->request->baseUrl . "/cropper/js/cropper.min.js");
+\app\assets\AppAsset::addJs($this, Yii::$app->request->baseUrl . "/cropper/js/script.js");
 
-\app\assets\AppAsset::addCss($this,Yii::$app->request->baseUrl."/cropper/css/ImgCropping.css");
-\app\assets\AppAsset::addCss($this,Yii::$app->request->baseUrl."/cropper/css/cropper.min.css");
+\app\assets\AppAsset::addCss($this, Yii::$app->request->baseUrl . "/cropper/css/ImgCropping.css");
+\app\assets\AppAsset::addCss($this, Yii::$app->request->baseUrl . "/cropper/css/cropper.min.css");
 ?>
 
 <?php $this->beginBody(); ?>
@@ -19,7 +19,7 @@ echo $this->render('../common/_layout');
         </div>
         <div class="am-u-lg-2 tpl-index-settings-button">
             <button type="button" class="page-header-button am-radius" onclick="refreshIframe()"><span
-                        class="am-icon-refresh"></span>
+                    class="am-icon-refresh"></span>
             </button>
         </div>
     </div>
@@ -41,9 +41,10 @@ echo $this->render('../common/_layout');
                     <form class="am-form tpl-form-border-form tpl-form-border-br" id="form">
                         <div class="am-form-group">
                             <label for="nickname" class="am-u-sm-3 am-form-label">昵称 <span
-                                        class="tpl-form-line-small-title">nickname</span></label>
+                                    class="tpl-form-line-small-title">nickname</span></label>
                             <div class="am-u-sm-9">
-                                <input type="text" class="tpl-form-input" id="nickname" name="nickname" placeholder="请输入昵称"
+                                <input type="text" class="tpl-form-input" id="nickname" name="nickname"
+                                       placeholder="请输入昵称"
                                        value="<?= $admin['nickname'] ?>">
                                 <!--                                <small>请填写标题文字10-20字左右。</small>-->
                             </div>
@@ -51,9 +52,10 @@ echo $this->render('../common/_layout');
 
                         <div class="am-form-group">
                             <label for="telphone" class="am-u-sm-3 am-form-label">手机号 <span
-                                        class="tpl-form-line-small-title">telphone</span></label>
+                                    class="tpl-form-line-small-title">telphone</span></label>
                             <div class="am-u-sm-9">
-                                <input type="text" class="tpl-form-input" id="telphone" name="telphone" placeholder="请输入手机号"
+                                <input type="text" class="tpl-form-input" id="telphone" name="telphone"
+                                       placeholder="请输入手机号"
                                        value="<?= $admin['telphone'] ?>">
                                 <!--<small>请填写标题文字10-20字左右。</small>-->
                             </div>
@@ -61,13 +63,14 @@ echo $this->render('../common/_layout');
 
                         <div class="am-form-group">
                             <label for="user-weibo" class="am-u-sm-3 am-form-label">头像 <span
-                                        class="tpl-form-line-small-title">Images</span></label>
+                                    class="tpl-form-line-small-title">Images</span></label>
                             <div class="am-u-sm-9">
                                 <div class="am-form-group am-form-file">
                                     <div class="tpl-form-file-img" style="margin-bottom: 1%">
                                         <img src="<?= $admin['avatar'] ?>" alt="" id="avatarView" width="300">
                                     </div>
-                                    <button type="button" class="am-btn am-btn-warning am-btn-sm" onclick="replaceImg()">
+                                    <button type="button" class="am-btn am-btn-warning am-btn-sm"
+                                            onclick="replaceImg()">
                                         <i class="am-icon-cloud-upload"></i> 上传头像
                                     </button>
                                 </div>
@@ -77,9 +80,10 @@ echo $this->render('../common/_layout');
 
                         <div class="am-form-group">
                             <label for="realname" class="am-u-sm-3 am-form-label">真实姓名 <span
-                                        class="tpl-form-line-small-title">telphone</span></label>
+                                    class="tpl-form-line-small-title">telphone</span></label>
                             <div class="am-u-sm-9">
-                                <input type="text" class="tpl-form-input" id="realname" name="realname" placeholder="请输入真实姓名"
+                                <input type="text" class="tpl-form-input" id="realname" name="realname"
+                                       placeholder="请输入真实姓名"
                                        value="<?= $admin['realname'] ?>">
                                 <!--<small>请填写标题文字10-20字左右。</small>-->
                             </div>
@@ -87,9 +91,11 @@ echo $this->render('../common/_layout');
 
                         <div class="am-form-group">
                             <label for="realname" class="am-u-sm-3 am-form-label">密码 <span
-                                        class="tpl-form-line-small-title">password</span></label>
+                                    class="tpl-form-line-small-title">password</span></label>
                             <div class="am-u-sm-9">
-                                <button type="button" class="am-btn am-btn-danger am-radius am-btn-block am-btn-sm" style="width: 12%" id="password">修改密码</button>
+                                <button type="button" class="am-btn am-btn-danger am-radius am-btn-block am-btn-sm"
+                                        style="width: 12%" id="password">修改密码
+                                </button>
                                 <small>点击修改密码。</small>
                             </div>
                         </div>
@@ -97,13 +103,15 @@ echo $this->render('../common/_layout');
                         <div class="am-form-group">
                             <label for="backup" class="am-u-sm-3 am-form-label">用户详情</label>
                             <div class="am-u-sm-9">
-                                <textarea class="" rows="10" id="backup" name="backup" placeholder="请输入内容"><?= $admin['backup'] ?></textarea>
+                                <textarea class="" rows="10" id="backup" name="backup"
+                                          placeholder="请输入内容"><?= $admin['backup'] ?></textarea>
                             </div>
                         </div>
 
                         <div class="am-form-group">
                             <div class="am-u-sm-9 am-u-sm-push-3">
-                                <button type="button" class="am-btn am-btn-primary tpl-btn-bg-color-success" id="submit">提交保存
+                                <button type="button" class="am-btn am-btn-primary tpl-btn-bg-color-success"
+                                        id="submit">提交保存
                                 </button>
                             </div>
                         </div>
@@ -119,10 +127,11 @@ echo $this->render('../common/_layout');
     <div class="tailoring-content">
         <div class="tailoring-content-one">
             <label title="上传图片" for="chooseImg" class="l-btn choose-btn">
-                <input type="file" accept="image/jpg,image/jpeg,image/png" name="file" id="chooseImg" class="hidden" onchange="selectImg(this)">
+                <input type="file" accept="image/jpg,image/jpeg,image/png" name="file" id="chooseImg" class="hidden"
+                       onchange="selectImg(this)">
                 选择图片
             </label>
-            <div class="close-tailoring"  onclick="closeTailor(this)">×</div>
+            <div class="close-tailoring" onclick="closeTailor(this)">×</div>
         </div>
         <div class="tailoring-content-two">
             <div class="tailoring-box-parcel">
@@ -150,9 +159,9 @@ echo $this->render('../common/_layout');
         <div class="am-modal-hd">骂骂酱提示您</div>
         <div class="am-modal-bd">
             正在修改密码哦
-<!--            <input type="text" class="am-modal-prompt-input" placeholder="请输入旧密码">-->
-<!--            <input type="text" class="am-modal-prompt-input" placeholder="请输入新密码">-->
-<!--            <input type="text" class="am-modal-prompt-input" placeholder="请重复新密码">-->
+            <!--            <input type="text" class="am-modal-prompt-input" placeholder="请输入旧密码">-->
+            <!--            <input type="text" class="am-modal-prompt-input" placeholder="请输入新密码">-->
+            <!--            <input type="text" class="am-modal-prompt-input" placeholder="请重复新密码">-->
             <form class="am-form editPasswordForm" style="margin-top: 2%">
                 <div class="am-input-group">
                     <span class="am-input-group-label"><i class="am-icon-lock am-icon-fw"></i></span>
@@ -182,45 +191,92 @@ echo $this->render('../common/_layout');
 <?php $this->endBody() ?>
 
 <script>
-    $(function() {
+    $(function () {
+        var passFlag = false;
         //修改密码弹框
-        $('#password').on('click', function() {
-            $(".am-modal-prompt-input").css("margin",'auto');
+        $('#password').on('click', function () {
+            $(".am-modal-prompt-input").css("margin", 'auto');
 
             $('#editPassword').modal({
                 relatedTarget: this,
-                onConfirm: function(e) {
-//                    alert('你输入的是：' + e.data || '')
-//                    console.log(e.data);
-                    console.log()
+                closeOnConfirm: false, //确定不关闭窗口
+                closeOnCancel: true, //取消关闭窗口
+                onConfirm: function (e) {
+                    console.log(e.data);
+
+                    if(!passFlag){
+                        alert('原密码错误');
+//                        myAlert('原密码错误');
+                        return;
+                    }
+                    var newpassword1 = e.data[1];
+                    var newpassword2 = e.data[2];
+
+                    if(newpassword1 == '' || newpassword2 == ''){
+                        alert('密码不能为空')
+                        return;
+                    }else if(newpassword1 != newpassword2){
+                        alert('两次输入密码不一致')
+                        return;
+                    }else{
+                        if(passFlag){
+                            $.post(
+                                "<?= \yii\helpers\Url::to(['admin/edit-password-ajax']) ?>",
+                                {
+                                    password: hex_md5(newpassword2),
+                                },
+                                function (data) {
+//                                    console.log(data);
+                                    myAlert(data.message);
+                                    return;
+                                },
+                                "json"
+                            );
+
+                            this.close(); //提交关闭窗口
+                        }
+
+                        return;
+                    }
 
                 },
-                onCancel: function(e) {
+                onCancel: function (e) {
                     return;
                 }
             });
         });
+
 
         //输入原密码弹窗失焦 查询密码是否正确
         $(".editPasswordForm").find('input').eq(0).blur(function () {
             var $this = $(this);
             var password = hex_md5($this.val());
 
-            $this.prev().removeClass('am-icon-lock');
-            $this.prev().addClass('am-icon-times');
-
             $.post(
                 "<?= \yii\helpers\Url::to(['admin/check-password-ajax']) ?>",
                 {
-                    password:password,
+                    password: password,
                 },
-                function(data){
+                function (data) {
                     //密码
-                    if(data==1000) {
+                    if (data.code == 1000) {
+                        passFlag = true; //可以提交
 
-                    }else{
-                        $this.prev().removeClass('am-icon-lock');
-//                        $this.prev().addClass('am-icon-times');
+                        $this.parent().removeClass('am-form-error');
+                        $this.parent().addClass('am-form-success');
+
+                        $this.prev().children().removeClass('am-icon-lock');
+                        $this.prev().children().removeClass('am-icon-times');
+                        $this.prev().children().addClass('am-icon-check');
+                    } else {
+                        passFlag = false;
+
+                        $this.parent().removeClass('am-form-success');
+                        $this.parent().addClass('am-form-error');
+
+                        $this.prev().children().removeClass('am-icon-lock');
+                        $this.prev().children().removeClass('am-icon-check');
+                        $this.prev().children().addClass('am-icon-times');
                     }
                 },
                 "json"
@@ -228,10 +284,11 @@ echo $this->render('../common/_layout');
 
         })
 
+
         //保存信息
         $("#submit").click(function () {
             const $this = $(this);
-            $this.attr('disabled',true);
+            $this.attr('disabled', true);
             var html = "<span class='am-icon-spinner am-icon-pulse'></span>&nbsp;&nbsp;正在保存...";
             $this.html(html)
 
@@ -247,19 +304,19 @@ echo $this->render('../common/_layout');
                 type: "POST",
                 data: form,
                 dataType: "json",
-                processData:false,
-                contentType:false,
+                processData: false,
+                contentType: false,
                 success: function (data) {
 //                console.log(data);
                     progress.done();//跳转进度结束
-                    $this.attr('disabled',false);
+                    $this.attr('disabled', false);
                     $this.html('提交保存')
                     myAlert(data.message);
 
                     //成功修改 common/_navBar头像和昵称
-                    if(data.status = 1000){
+                    if (data.status = 1000) {
                         window.parent.$("#admin-nickname").text(data.data.nickname);
-                        window.parent.$("#admin-avatar").attr('src',data.data.avatar);
+                        window.parent.$("#admin-avatar").attr('src', data.data.avatar);
 
                     }
 
