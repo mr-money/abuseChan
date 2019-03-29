832acd7b615bf971bef4d690a21b82d69bfbff5e
qianjing <740363495@qq.com>
2019-03-29 14:49:17 +0800

    admin导航栏配置文件

--- config/params.php
+++ config/params.php
@@ -1,4 +1,5 @@
 <?php
+$adminMenu = require __DIR__ . '/adminMenu.php'; //后台左侧导航栏
 
 return [
     'adminEmail' => 'admin@example.com',
@@ -10,80 +11,7 @@ return [
     ),
 
     //后台左侧导航栏
-    'adminMenu' => array(
-        'Components' => array(
-            'navInfo' => '附加组件',
-            'navLink' => array(
-                array(
-                    'name' => '主页概览',
-                    'url' => "admin/home",
-                    'logo' => "am-icon-home sidebar-nav-link-logo",
-                    'navChild' => array(),
-                ),
-                array(
-                    'name' => '网站主页',
-                    'url' => "admin/home",
-                    'logo' => "am-icon-home sidebar-nav-link-logo",
-                    'navChild' => array(),
-                ),
-                array(
-                    'name' => '微信菜单',
-                    'url' => "admin/menu",
-                    'logo' => "am-icon-weixin sidebar-nav-link-logo",
-                    'navChild' => array(),
-                ),
-                array(
-                    'name' => '表格',
-                    'url' => "",
-                    'logo' => "am-icon-table sidebar-nav-link-logo",
-                    'navChild' => array(),
-                ),
-                array(
-                    'name' => '表单',
-                    'url' => "",
-                    'logo' => "am-icon-wpforms sidebar-nav-link-logo",
-                    'navChild' => array(),
-                ),
-
-            ),
-        ),
-
-        'Page' => array(
-            'navInfo' => '常用页面',
-            'navLink' => array(
-                array(
-                    'name' => '数据列表',
-                    'url' => null,  //存在子页面 不生成url 设置为null或false
-                    'logo' => "am-icon-table sidebar-nav-link-logo",
-                    'navChild' => array(
-                        array(
-                            'name' => '文字列表',
-                            'url' => "",
-                            'logo' => "am-icon-table sidebar-nav-link-logo",
-                        ),
-                        array(
-                            'name' => '图文列表',
-                            'url' => "",
-                            'logo' => "am-icon-angle-right sidebar-nav-link-logo",
-                        ),
-                    ),
-                ),
-                array(
-                    'name' => '注册',
-                    'url' => "",
-                    'logo' => "am-icon-clone sidebar-nav-link-logo",
-                    'navChild' => array(),
-                ),
-                array(
-                    'name' => '404错误',
-                    'url' => "",
-                    'logo' => "am-icon-tv sidebar-nav-link-logo",
-                    'navChild' => array(),
-                ),
-            ),
-        ),
-    ),
-
+    'adminMenu' => $adminMenu,
 
     // 微信配置 具体可参考EasyWechat
     'wechatConfig' => [
