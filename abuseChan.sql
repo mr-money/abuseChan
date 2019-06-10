521b1ab6c1a265ff6829571558a12cc38d4efacf
qianjing <740363495@qq.com>
2019-06-10 17:43:56 +0800

    数据库备份

--- abuseChan.sql
+++ abuseChan.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog 企业版 - MySQL GUI v7.14 
-MySQL - 5.5.53 : Database - abusechan
+MySQL - 5.5.60-MariaDB : Database - abusechan
 *********************************************************************
 */
 
@@ -47,7 +47,7 @@ CREATE TABLE `abuse_wx_user` (
   `openid` varchar(255) NOT NULL,
   `nickname` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
   `avatar` varchar(255) DEFAULT NULL,
-  `sex` tinyint(4) unsigned DEFAULT NULL,
+  `sex` tinyint(4) unsigned DEFAULT '1',
   `subscribe_time` int(10) DEFAULT NULL,
   `address` varchar(255) DEFAULT NULL COMMENT '地址',
   `realname` varchar(255) DEFAULT NULL COMMENT '姓名',
@@ -55,15 +55,28 @@ CREATE TABLE `abuse_wx_user` (
   `city` char(32) DEFAULT NULL COMMENT '城市',
   `province` char(32) DEFAULT NULL COMMENT '省市',
   `country` char(32) DEFAULT NULL COMMENT '国家',
-  `is_subscribe` tinyint(4) DEFAULT '1',
-  `createTime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `uptateTime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `backup` varchar(255) DEFAULT NULL COMMENT '保留字段',
+  `state` int(11) DEFAULT '0' COMMENT '状态',
+  `create_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `uptate_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 
 /*Data for the table `abuse_wx_user` */
 
-insert  into `abuse_wx_user`(`id`,`openid`,`nickname`,`avatar`,`sex`,`subscribe_time`,`address`,`realname`,`tel`,`city`,`province`,`country`,`is_subscribe`,`createTime`,`uptateTime`) values (1,'1','昵称','123123123',1,123123123,'上海上海','钱京','123456','上海','上海','中国',1,'2019-01-24 17:06:11','2019-01-24 17:06:11');
+insert  into `abuse_wx_user`(`id`,`openid`,`nickname`,`avatar`,`sex`,`subscribe_time`,`address`,`realname`,`tel`,`city`,`province`,`country`,`backup`,`state`,`create_at`,`uptate_at`) values (1,'1','昵称','http://thirdwx.qlogo.cn/mmopen/yTRjlqxLq89we6kVqcyQ3frotPvKYZNLrMXnRuOeYqicjZiaO9YRmBEicI1gl50bpZOPCxtZIakZVyHxquqq0iaEjbtXnvqebJL6/132',1,123123123,'上海上海','钱京','123456','上海','上海','中国','1',0,'2019-01-24 17:06:11','2019-01-24 17:06:11');
+insert  into `abuse_wx_user`(`id`,`openid`,`nickname`,`avatar`,`sex`,`subscribe_time`,`address`,`realname`,`tel`,`city`,`province`,`country`,`backup`,`state`,`create_at`,`uptate_at`) values (2,'2','小香菇','http://thirdwx.qlogo.cn/mmopen/Y5zKr12DSYM0IyMdcickaFj8pvyJSDtG6wzxRjmmVnDZYvfuSnYaoa7vYp7dVkCspmPiaRib7pzvMQZqVrZQzwBS3gz2R8dyhg1/132',1,1233,'新北','啦啦啦','123123','常州','江苏','中国','222222',0,'2019-05-24 17:06:11','2019-06-24 17:06:11');
+insert  into `abuse_wx_user`(`id`,`openid`,`nickname`,`avatar`,`sex`,`subscribe_time`,`address`,`realname`,`tel`,`city`,`province`,`country`,`backup`,`state`,`create_at`,`uptate_at`) values (3,'3','1',NULL,1,NULL,NULL,NULL,'4',NULL,NULL,NULL,NULL,0,'0000-00-00 00:00:00','0000-00-00 00:00:00');
+insert  into `abuse_wx_user`(`id`,`openid`,`nickname`,`avatar`,`sex`,`subscribe_time`,`address`,`realname`,`tel`,`city`,`province`,`country`,`backup`,`state`,`create_at`,`uptate_at`) values (4,'4','2',NULL,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,'0000-00-00 00:00:00','0000-00-00 00:00:00');
+insert  into `abuse_wx_user`(`id`,`openid`,`nickname`,`avatar`,`sex`,`subscribe_time`,`address`,`realname`,`tel`,`city`,`province`,`country`,`backup`,`state`,`create_at`,`uptate_at`) values (5,'5','3',NULL,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,'0000-00-00 00:00:00','0000-00-00 00:00:00');
+insert  into `abuse_wx_user`(`id`,`openid`,`nickname`,`avatar`,`sex`,`subscribe_time`,`address`,`realname`,`tel`,`city`,`province`,`country`,`backup`,`state`,`create_at`,`uptate_at`) values (6,'6','4',NULL,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,'0000-00-00 00:00:00','0000-00-00 00:00:00');
+insert  into `abuse_wx_user`(`id`,`openid`,`nickname`,`avatar`,`sex`,`subscribe_time`,`address`,`realname`,`tel`,`city`,`province`,`country`,`backup`,`state`,`create_at`,`uptate_at`) values (7,'7',NULL,NULL,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,'0000-00-00 00:00:00','0000-00-00 00:00:00');
+insert  into `abuse_wx_user`(`id`,`openid`,`nickname`,`avatar`,`sex`,`subscribe_time`,`address`,`realname`,`tel`,`city`,`province`,`country`,`backup`,`state`,`create_at`,`uptate_at`) values (11,'8',NULL,NULL,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,'0000-00-00 00:00:00','0000-00-00 00:00:00');
+insert  into `abuse_wx_user`(`id`,`openid`,`nickname`,`avatar`,`sex`,`subscribe_time`,`address`,`realname`,`tel`,`city`,`province`,`country`,`backup`,`state`,`create_at`,`uptate_at`) values (12,'9',NULL,NULL,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,'0000-00-00 00:00:00','0000-00-00 00:00:00');
+insert  into `abuse_wx_user`(`id`,`openid`,`nickname`,`avatar`,`sex`,`subscribe_time`,`address`,`realname`,`tel`,`city`,`province`,`country`,`backup`,`state`,`create_at`,`uptate_at`) values (14,'11',NULL,NULL,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,'0000-00-00 00:00:00','0000-00-00 00:00:00');
+insert  into `abuse_wx_user`(`id`,`openid`,`nickname`,`avatar`,`sex`,`subscribe_time`,`address`,`realname`,`tel`,`city`,`province`,`country`,`backup`,`state`,`create_at`,`uptate_at`) values (15,'12',NULL,NULL,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,'0000-00-00 00:00:00','0000-00-00 00:00:00');
+insert  into `abuse_wx_user`(`id`,`openid`,`nickname`,`avatar`,`sex`,`subscribe_time`,`address`,`realname`,`tel`,`city`,`province`,`country`,`backup`,`state`,`create_at`,`uptate_at`) values (16,'13',NULL,NULL,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,'0000-00-00 00:00:00','0000-00-00 00:00:00');
+insert  into `abuse_wx_user`(`id`,`openid`,`nickname`,`avatar`,`sex`,`subscribe_time`,`address`,`realname`,`tel`,`city`,`province`,`country`,`backup`,`state`,`create_at`,`uptate_at`) values (17,'14',NULL,NULL,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,'0000-00-00 00:00:00','0000-00-00 00:00:00');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
