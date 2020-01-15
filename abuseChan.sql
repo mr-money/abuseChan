9519de8d90e757abc4dff74251987760f0884dcf
qianjing <740363495@qq.com>
2020-01-15 10:15:09 +0800

    测试数据库数据修改

--- abuseChan.sql
+++ abuseChan.sql
@@ -2,7 +2,8 @@
 SQLyog 企业版 - MySQL GUI v7.14 
 MySQL - 5.5.60-MariaDB : Database - abusechan
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -32,11 +33,11 @@ CREATE TABLE `abuse_admin_user` (
   `updated_at` timestamp NULL DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `admin_users_telphone_unique` (`telphone`)
-) ENGINE=MyISAM AUTO_INCREMENT=1001 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1001 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `abuse_admin_user` */
 
-insert  into `abuse_admin_user`(`id`,`nickname`,`telphone`,`password`,`realname`,`avatar`,`remember_token`,`backup`,`created_at`,`updated_at`) values (1000,'admin','18651984625','96E79218965EB72C92A549DD5A330112','钱京',NULL,NULL,NULL,'2019-01-26 11:18:36','2019-01-26 11:18:36');
+insert  into `abuse_admin_user`(`id`,`nickname`,`telphone`,`password`,`realname`,`avatar`,`remember_token`,`backup`,`created_at`,`updated_at`) values (1001,'admin','18651984625','96E79218965EB72C92A549DD5A330112','钱京',NULL,NULL,NULL,'2019-01-26 11:18:36','2019-01-26 11:18:36');
 
 /*Table structure for table `abuse_wx_user` */
 
@@ -60,11 +61,11 @@ CREATE TABLE `abuse_wx_user` (
   `create_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `uptate_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1001 DEFAULT CHARSET=utf8;
 
 /*Data for the table `abuse_wx_user` */
 
-insert  into `abuse_wx_user`(`id`,`openid`,`nickname`,`avatar`,`sex`,`subscribe_time`,`address`,`realname`,`tel`,`city`,`province`,`country`,`backup`,`state`,`create_at`,`uptate_at`) values (1,'1','昵称','http://thirdwx.qlogo.cn/mmopen/yTRjlqxLq89we6kVqcyQ3frotPvKYZNLrMXnRuOeYqicjZiaO9YRmBEicI1gl50bpZOPCxtZIakZVyHxquqq0iaEjbtXnvqebJL6/132',1,123123123,'上海上海','钱京','123456','上海','上海','中国','1',0,'2019-01-24 17:06:11','2019-01-24 17:06:11');
+insert  into `abuse_wx_user`(`id`,`openid`,`nickname`,`avatar`,`sex`,`subscribe_time`,`address`,`realname`,`tel`,`city`,`province`,`country`,`backup`,`state`,`create_at`,`uptate_at`) values (1001,'1','昵称','http://thirdwx.qlogo.cn/mmopen/yTRjlqxLq89we6kVqcyQ3frotPvKYZNLrMXnRuOeYqicjZiaO9YRmBEicI1gl50bpZOPCxtZIakZVyHxquqq0iaEjbtXnvqebJL6/132',1,123123123,'上海上海','钱京','123456','上海','上海','中国','1',0,'2019-01-24 17:06:11','2019-01-24 17:06:11');
 insert  into `abuse_wx_user`(`id`,`openid`,`nickname`,`avatar`,`sex`,`subscribe_time`,`address`,`realname`,`tel`,`city`,`province`,`country`,`backup`,`state`,`create_at`,`uptate_at`) values (2,'2','小香菇','http://thirdwx.qlogo.cn/mmopen/Y5zKr12DSYM0IyMdcickaFj8pvyJSDtG6wzxRjmmVnDZYvfuSnYaoa7vYp7dVkCspmPiaRib7pzvMQZqVrZQzwBS3gz2R8dyhg1/132',1,1233,'新北','啦啦啦','123123','常州','江苏','中国','222222',0,'2019-05-24 17:06:11','2019-06-24 17:06:11');
 insert  into `abuse_wx_user`(`id`,`openid`,`nickname`,`avatar`,`sex`,`subscribe_time`,`address`,`realname`,`tel`,`city`,`province`,`country`,`backup`,`state`,`create_at`,`uptate_at`) values (3,'3','1',NULL,1,NULL,NULL,NULL,'4',NULL,NULL,NULL,NULL,0,'0000-00-00 00:00:00','0000-00-00 00:00:00');
 insert  into `abuse_wx_user`(`id`,`openid`,`nickname`,`avatar`,`sex`,`subscribe_time`,`address`,`realname`,`tel`,`city`,`province`,`country`,`backup`,`state`,`create_at`,`uptate_at`) values (4,'4','2',NULL,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,'0000-00-00 00:00:00','0000-00-00 00:00:00');
