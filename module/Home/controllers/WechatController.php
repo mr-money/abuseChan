69128f4a46dce6c97704f623780c8d793ea4bec0
qianjing <740363495@qq.com>
2020-01-16 16:08:24 +0800

    微信用户

--- module/Home/controllers/WechatController.php
+++ module/Home/controllers/WechatController.php
@@ -33,7 +33,7 @@ class WechatController extends Controller
 //        var_dump(Yii::$app->wechat->app->oauth->redirect()->getTargetUrl());die;
 
         var_dump(Yii::$app->request->get('code'));
-        if (Yii::$app->wechat->isWechat && !Yii::$app->wechat->isAuthorized()) {
+        if (Yii::$app->wechat->getIsWechat() && !Yii::$app->wechat->isAuthorized()) {
             var_dump(Yii::$app->wechat->app->oauth->redirect()->getTargetUrl());
             return Yii::$app->wechat->authorizeRequired()->send();die;
 //            $response = Yii::$app->wechat->authorizeRequired();
